503ecb9f6caff842dd8d499396142e087c184ebf
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2022-05-05 17:04:32 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,19 @@
 if [[ "$cuda_compiler_version" == "None" ]]; then
   export FORCE_CUDA=0
 else
+  export CUDA_NVCC_FLAGS="$CUDA_NVCC_FLAGS -O3"
+  if [[ ${cuda_compiler_version} == 10.* ]]; then
+      export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5"
+  elif [[ ${cuda_compiler_version} == 11.0* ]]; then
+      export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0"
+  elif [[ ${cuda_compiler_version} == 11.1* ]]; then
+      export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6"
+  elif [[ ${cuda_compiler_version} == 11.2* ]]; then
+      export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6"
+  else
+      echo "unsupported cuda version. edit build.sh"
+      exit 1
+  fi
   export FORCE_CUDA=1
 fi
 
