6c33a1273adb375c563e5c9d751daf73b6f2d07d
Mark Harfouche <mark.harfouche@gmail.com>
2024-03-07 20:22:59 -0500

    Increase minimum cudaarch

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,23 +8,18 @@ if [[ "$cuda_compiler_version" == "None" ]]; then
   export FORCE_CUDA=0
 else
   export CUDA_NVCC_FLAGS="$CUDA_NVCC_FLAGS -O3"
-  if [[ ${cuda_compiler_version} == 9.0* ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;7.0+PTX"
-  elif [[ ${cuda_compiler_version} == 9.2* ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0+PTX"
-  elif [[ ${cuda_compiler_version} == 10.* ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5+PTX"
-  elif [[ ${cuda_compiler_version} == 11.0* ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0+PTX"
+  # They use __hadd2 which seems to require CUDA_ARCH at least 8.0
+  if [[ ${cuda_compiler_version} == 11.0* ]]; then
+      export TORCH_CUDA_ARCH_LIST="8.0+PTX"
   elif [[ ${cuda_compiler_version} == 11.1* ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+      export TORCH_CUDA_ARCH_LIST="8.0;8.6+PTX"
   elif [[ ${cuda_compiler_version} == 11.2* ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+      export TORCH_CUDA_ARCH_LIST="8.0;8.6+PTX"
   elif [[ ${cuda_compiler_version} == 11.8 ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
+      export TORCH_CUDA_ARCH_LIST="8.0;8.6;8.9+PTX"
       export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
   elif [[ ${cuda_compiler_version} == 12.0 ]]; then
-      export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
+      export TORCH_CUDA_ARCH_LIST="8.0;8.6;8.9;9.0+PTX"
       # $CUDA_HOME not set in CUDA 12.0. Using $PREFIX
       export CUDA_TOOLKIT_ROOT_DIR="${PREFIX}"
   else
